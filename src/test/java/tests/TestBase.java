06e7c55c7760feaf4bda8b6ddf75e9da3188d553
Ir4fin <messu91@gmail.com>
2022-11-01 20:15:37 +0500

    added attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -9,5 +11,10 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Step's screenshot");
     }
 }
