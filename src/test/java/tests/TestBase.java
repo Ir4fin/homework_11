a1dd106f1aaf31c80422565a7dce636d29ff7c0e
Ir4fin <messu91@gmail.com>
2022-11-01 20:18:48 +0500

    cahnged attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import helpers.Attach;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -12,9 +13,9 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-
+    @Step("Делаем скриншот результата")
     @AfterEach
     void addAttachments(){
-        Attach.screenshotAs("Step's screenshot");
+        Attach.screenshotAs("Result screenshot");
     }
 }
